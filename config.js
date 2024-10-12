7f7a3834bc93481aa06886ce4d25428ab61b7b21
KirillBychkov <bychkovkirill@live.com>
2024-10-12 10:32:31 +0300

    init

--- config.js
+++ config.js
@@ -11,3 +11,73 @@ export const config = {
     prevCommand: null,
     page: 0,
 };
+
+/* -------------------------------------------- */
+
+// http://64.225.108.221:3001  FIRST
+// {
+//     "slugs": [
+//         "altlayer",
+//         "mask-network",
+//         "open-campus",
+//         "zetachain",
+//         "blast",
+//         "illuvium",
+//         "biconomy",
+//         "arkham",
+//         "gmx",
+//         "space-id",
+//         "meme",
+//         "basic-attention-token",
+//         "celo",
+//         "cardano",
+//         "bnb"
+//     ]
+// }
+
+/* -------------------------------------------- */
+
+// http://164.90.189.154:3001  SECOND
+// {
+//     "slugs": [
+//         "sui",
+//         "pepe",
+//         "artificial-superintelligence-alliance",
+//         "bittensor",
+//         "aptos",
+//         "kaspa",
+//         "render",
+//         "stellar",
+//         "stacks",
+//         "monero",
+//         "dogwifhat",
+//         "arbitrum",
+//         "hedera",
+//         "aave",
+//         "optimism-ethereum"
+//     ]
+// }
+
+/* -------------------------------------------- */
+
+// http://167.99.250.107:3001  THIRD
+// {
+//     "slugs": [
+//         "fantom",
+//         "the-graph",
+//         "sei",
+//         "floki-inu",
+//         "pyth-network",
+//         "jupiter-ag",
+//         "popcat-sol",
+//         "flow",
+//         "algorand",
+//         "neo",
+//         "notcoin",
+//         "starknet-token",
+//         "ordi",
+//         "gala",
+//         "ethena"
+//     ]
+// }
+
--- cron-jobs.js
+++ cron-jobs.js
@@ -8,18 +8,36 @@ const state = ["🔴", "🟠", "🟢"];
 export function startCronJobs() {
   const every5Job = new CronJob(
     '*/10 * * * * *', async function () {
-        const res = await got.get("http://64.225.108.221:3001/status");
         console.log('Get statuses...');
-        const data = JSON.parse(res.body);
+        const [res1, res2, res3] = await Promise.all([
+            got.get("http://64.225.108.221:3001/status"),
+            got.get("http://164.90.189.154:3001/status"),
+            got.get("http://167.99.250.107:3001/status"),
+        ]);
+        const data1 = JSON.parse(res1.body), data2 = JSON.parse(res2.body), data3 = JSON.parse(res3.body);
+        const data = {
+            percentage: [...data1.percentage, ...data2.percentage, ...data3.percentage],
+            response: { ...data1.response, ...data2.response, ...data3.response },
+            request: {
+                cex: data1.request.cex,
+                pairs: data1.request.pairs,
+                workers: data1.request.workers + data2.request.workers + data3.request.workers,
+                coins : [...data1.request.coins, ...data2.request.coins, ...data3.request.coins ]
+            },
+        };
+
         data.percentage.forEach(elem => {
             (async () => {
-                const pos = data.response[elem.chain];
-                console.log({ ...elem });
-                if(+elem.value > 0.4) {
+                const pos = data.response[elem.chain],
+                    timeCheckedBuy = Math.abs(+new Date() - +new Date(pos.buy.time)),
+                    timeCheckedSell = Math.abs(+new Date() - +new Date(pos.sell.time));
+                console.log({ ...elem, timeCheckedBuy, timeCheckedSell });
+                if(+elem.value > 0.5 && timeCheckedSell < 25000 && timeCheckedSell < 25000) {
                     const text =
                         "*" + elem.chain.toUpperCase() + " " + state[2] +
                         "\nBuy " + "CEX" + " - Sell " + "CEX" +
                         "\n" + (new Date()).toString().split('G')[0] + "*\n" +
+                        "\n" + ("Checked " + (Math.round(Math.max(timeCheckedSell, timeCheckedBuy) / 1000)) +  " seconds ago...") +
                         "\n" + +elem.value + "%\n" +
 
                         "\n***buy***\nprice: " + pos.buy.asks.price +
